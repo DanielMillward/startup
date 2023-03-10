918aa4e7d60925a9ccee66f34770653b4ab9a258
danielfm <dfmillward02@gmail.com>
2023-03-10 15:50:40 -0700

    Add basic new game logic

--- js/index.js
+++ js/index.js
@@ -6,22 +6,36 @@ const signupButton = anonButtons[1].querySelector('button');
 // Check if the user is logged in
 if (localStorage.getItem('currUser')) {
   anonButtons.forEach(button => button.style.display = 'none');
+  const navbar = document.querySelector('.navbar-nav');
+  //make profile button
+  const profileButton = document.createElement('button');
+  profileButton.type = 'button';
+  profileButton.classList.add('btn', 'btn-dark', 'rounded-pill', 'py-0');
+  profileButton.innerText = 'View Profile';
+  profileButton.addEventListener('click', () => {
+    window.location.href = "profile.html";
+  });
+  const profileListItem = document.createElement('li');
+  profileListItem.classList.add('nav-item', 'p-2', 'anonButton');
+  profileListItem.appendChild(profileButton);
+  navbar.appendChild(profileListItem);
+
   // Make sign out button
   const signoutButton = document.createElement('button');
   signoutButton.type = 'button';
   signoutButton.classList.add('btn', 'btn-dark', 'rounded-pill', 'py-0');
   signoutButton.innerText = 'Sign Out';
-
   signoutButton.addEventListener('click', () => {
     localStorage.removeItem('currUser');
     location.reload();
   });
-  // Add the "sign out" button to the navbar
   const signoutListItem = document.createElement('li');
   signoutListItem.classList.add('nav-item', 'p-2', 'anonButton');
   signoutListItem.appendChild(signoutButton);
-  const navbar = document.querySelector('.navbar-nav');
+  
   navbar.appendChild(signoutListItem);
+
+
 } else {
   // Show the "login" and "signup" buttons
   loginButton.style.display = 'block';
--- js/newgame.js
+++ js/newgame.js
@@ -1,13 +1,22 @@
 document.getElementById("noUser").style.display = "none"; 
-
 const currUser = localStorage.getItem('currUser');
-console.log("Curr user is" + currUser)
+console.log("Curr user is " + currUser)
+
 // if no current user, don't display page
 if (!currUser) {
 document.getElementById("noUser").style.display = "block"; 
-document.getElementById("newGameDiv").style.display = "none"; 
+document.getElementById("profileDiv").style.display = "none"; 
 } else {
-// get current user data
+//Check all fields filled out
+
+
+const gameNameInput = document.getElementById('gameName');
+const bigBlindInput = document.getElementById('bigBlind');
+const startingStackOneInput = document.getElementById('stackOne');
+const startingStackTwoInput = document.getElementById('stackTwo');
+const signupBtn = document.getElementById('submitButton');
+
+//user logged in and all fields made, time to add the game to the user list
 const users = JSON.parse(localStorage.getItem("users"));
 const currentUser = users.find(user => user.username === currUser);
 
@@ -16,15 +25,28 @@ if (!currentUser) {
     document.getElementById("newGameDiv").style.display = "none"; 
 }
 
-document.getElementById('username').textContent = currentUser.username;
-document.getElementById('accountDate').textContent = currentUser.accountDate;
-document.getElementById('gamesPlayed').textContent = currentUser.gamesPlayed;
-document.getElementById('totalMoneyWon').textContent = currentUser.moneyWon;
-document.getElementById('profilePic').src = "https://robohash.org/" + encodeURIComponent(currentUser.username) + ".png"
 
-currentUser.games.forEach((value) => {
-    console.log(value);
-    });
+signupBtn.addEventListener('click', (event) => {
+    event.preventDefault(); // Needed for some reason
+
+    console.log('Game Name: ', gameNameInput.value);
+    console.log('Big Blind: ', bigBlindInput.value);
+    console.log('Starting Stack for Player 1: ', startingStackOneInput.value);
+    console.log('Starting Stack for Player 2: ', startingStackTwoInput.value);
+    if (gameNameInput.value && !isNaN(bigBlindInput.value) && !isNaN(startingStackOneInput.value) && !isNaN(startingStackTwoInput.value)) {
+      bb = parseInt(bigBlindInput.value)
+      stackOne = parseInt(startingStackOneInput.value)
+      stackTwo = parseInt(startingStackTwoInput.value)
+      if (bb < stackOne && bb < stackTwo) {
+        alert("all good!")
+      } else {
+        alert("BB needs to be smaller than either stack.")
+      }
+    } else {
+      alert("Please fill in all fields, with the big blind/stacks being a number and the big blind less than either stack.");
+    }
+  });
+
 }
 
 
@@ -36,7 +58,8 @@ const loginButton = anonButtons[0].querySelector('button');
 const signupButton = anonButtons[1].querySelector('button');
 
 // Check if the user is logged in
-if (localStorage.getItem('currUser')) {
+if (currUser) {
+    console.log("theres a user")
   anonButtons.forEach(button => button.style.display = 'none');
   // Make sign out button
   const signoutButton = document.createElement('button');
@@ -54,8 +77,9 @@ if (localStorage.getItem('currUser')) {
   signoutListItem.appendChild(signoutButton);
   const navbar = document.querySelector('.navbar-nav');
   navbar.appendChild(signoutListItem);
+
 } else {
-  // Show the "login" and "signup" buttons
+  // user not logged in
   loginButton.style.display = 'block';
   signupButton.style.display = 'block';
 }

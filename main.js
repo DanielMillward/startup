56dd899879fdc3b0cf1bd7d262cf9178847a2a30
danielfm <dfmillward02@gmail.com>
2023-03-30 00:18:19 -0600

    Add signup functionality

--- main.js
+++ main.js
@@ -4,28 +4,63 @@ const DB = require('./database.js');
 
 const app = express();
 app.use(cookieParser());
+app.use(express.json());
 
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
+//check if logged in with valid token. If so, redirect to profile
 const loggedInMiddleware = async (req, res, next) => {
     if (req.cookies.authToken) {
         const user = await DB.getUserByToken(req.cookies.authToken);
         if (user) {
-            res.redirect('/api/badlogin');
+            res.redirect('/profile');
         } 
-    } 
+    }
     next();
 }
 
+function bothUserNameAndPasswordPresent(req) {
+    if (!req.body) {
+        console.log("no body");
+        return false;
+    }
+    if (req.body.email && req.body.password) {
+        return true;
+    }
+    console.log(req.body);
+    return false;
+}
+
+//just api landing page
 apiRouter.get('/', loggedInMiddleware, (req, res) => {
     res.send('welcome to a simple HTTP cookie server');
 });
 
-apiRouter.get('/badlogin', (req, res) => {
-    res.send('Had login when you werent suppoesd to lol');
+//for temporary redirect
+apiRouter.get('/profile', (req, res) => {
+    res.send('At your profile!');
 });
+
+//for making users
+apiRouter.post('/auth/create', async (req, res) => {
+    if (!bothUserNameAndPasswordPresent(req)) {
+        res.status(413).send({msg: 'either username or password not here'});
+    } else {
+        if (await DB.getUser(req.body.email)) {
+            res.status(409).send({ msg: 'Existing user' });
+          } else {
+            const user = await DB.createUser(req.body.email, req.body.password);
+            // Set the cookie
+            res.cookie(res,user.token);
+            res.send({
+              id: user._id,
+            });
+          }
+    }
+    
+  });
 
 apiRouter.get('/setcookie', (req, res) => {
     res.cookie(`authToken`,`37664ccb-b5f8-4ab6-ab76-876c707b65f1`);
@@ -37,18 +72,6 @@ apiRouter.get('/getcookie', (req, res) => {
     res.send(req.cookies);
 });
 
-apiRouter.get('/isLoggedIn', async (req, res) => {
-    if (req.cookies.authToken) {
-        const user = await DB.getUserByToken(req.cookies.authToken);
-        if (user) {
-            res.send(user);
-        } else {
-            res.send("have token, no matching user");
-        }
-    } else {
-        res.send('no authtoken found');
-    }
-});
 
 
 

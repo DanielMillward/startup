1423e682d96dbe373129c24d5ae4ef42b0c5feea
danielfm <dfmillward02@gmail.com>
2023-03-30 00:42:52 -0600

    Add basic login/logout api calls

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const DB = require('./database.js');
+const bcrypt = require('bcrypt');
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.static('public'));
 
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-//check if logged in with valid token. If so, redirect to profile
+//check if have valid token. If so, redirect to profile
 const loggedInMiddleware = async (req, res, next) => {
-    if (req.cookies.authToken) {
-        const user = await DB.getUserByToken(req.cookies.authToken);
+    if (req.cookies.userToken) {
+        const user = await DB.getUserByToken(req.cookies.userToken);
         if (user) {
-            res.redirect('/profile');
-        } 
+            console.log("got user...");
+            res.status(422).send({msg: 'need to redirect to profile'});
+            return;
+        } else {
+            console.log("no matching user found");
+        }
     }
     next();
 }
@@ -38,11 +44,6 @@ apiRouter.get('/', loggedInMiddleware, (req, res) => {
     res.send('welcome to a simple HTTP cookie server');
 });
 
-//for temporary redirect
-apiRouter.get('/profile', (req, res) => {
-    res.send('At your profile!');
-});
-
 //for making users
 apiRouter.post('/auth/create', async (req, res) => {
     if (!bothUserNameAndPasswordPresent(req)) {
@@ -53,26 +54,36 @@ apiRouter.post('/auth/create', async (req, res) => {
           } else {
             const user = await DB.createUser(req.body.email, req.body.password);
             // Set the cookie
-            res.cookie(res,user.token);
+            res.cookie("userToken",user.token);
             res.send({
               id: user._id,
             });
           }
     }
-    
   });
 
-apiRouter.get('/setcookie', (req, res) => {
-    res.cookie(`authToken`,`37664ccb-b5f8-4ab6-ab76-876c707b65f1`);
-    res.send('Cookie have been saved successfully');
+// GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+    if (!bothUserNameAndPasswordPresent(req)) {
+        res.status(413).send({msg: 'either username or password not here'});
+    } else {
+        const user = await DB.getUser(req.body.email);
+        if (user) {
+            if (await bcrypt.compare(req.body.password, user.password)) {
+                res.cookie("userToken", user.token);
+                res.redirect('/profile.html');
+                return;
+            }
+        }
+        res.status(401).send({ msg: 'Unauthorized' });
+    }
 });
 
-apiRouter.get('/getcookie', (req, res) => {
-    console.log(req.cookies)
-    res.send(req.cookies);
+// Logout
+apiRouter.delete('/auth/logout', (_req, res) => {
+    res.clearCookie("userToken");
+    res.status(204).end();
 });
-
-
 
 
 
